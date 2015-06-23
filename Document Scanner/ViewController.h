276e74bf59f136fbf710f7236d4546d0eb14fcf9
kazuh1ko <gonchigsuren_bayarbaatar@unimedia.co.jp>
2015-06-23 15:30:01 +0900

    Initial Commit

---
+++ Document Scanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Document Scanner
+//
+//  Created by Unimedia on 6/23/15.
+//  Copyright (c) 2015 kazuh1ko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
